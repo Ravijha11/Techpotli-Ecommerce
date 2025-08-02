09fb78265f229025bdfb21062cc90d8330384157
Sandesh Upadhyay <upadhyaysandesh28@gmail.com>
2025-08-02 22:36:13 +0530

    Email & contact updated

--- Frontend/components/layout/Header/ContactInfo.tsx
+++ Frontend/components/layout/Header/ContactInfo.tsx
@@ -7,12 +7,12 @@ export default function ContactInfo() {
     <div className="flex items-center space-x-4">
       <span className="flex items-center space-x-1">
         <Mail className="w-4 h-4" />
-        <span>Mail webzedcontact@gmail.com</span>
+        <span>Mail info@techpotli.com</span>
       </span>
       <span className="text-gray-400">|</span>
       <span className="flex items-center space-x-1">
         <Phone className="w-4 h-4" />
-        <span>Helpline 4534345656</span>
+        <span>Helpline 01147200987</span>
       </span>
     </div>
   )
